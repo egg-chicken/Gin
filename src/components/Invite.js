e14a33ce3cae78c1e8ff1399a965918d4bb8821b
Ken Eguchi <no.eggchicken@gmail.com>
2018-12-01 23:40:47 +0900

    Add index state

--- src/components/Invite.js
+++ src/components/Invite.js
@@ -5,33 +5,34 @@ import Page from './Page'
 import Character from './invite/Character'
 import Button from '@material-ui/core/Button'
 
-let currentIndex = 0
 
-const handleSwipe = (direction) => {
-  if(direction === "left") {
-    currentIndex = (currentIndex + 1) % 4
-  } else{
-    currentIndex = (currentIndex === 0) ? 3 : currentIndex - 1
+export default class Invite extends React.Component {
+  state = { index: 0 }
+
+  handleSwipe = (index) => {
+    this.setState({index: index})
   }
-}
 
-export default ({onClick, characters}) => {
-  return (
-    <Page>
-      <div className="InviteSlider">
-        <Slider dots infinite speed={300} onSwipe={handleSwipe}>
-          { characters.map(c => <Character key={c.id} character={c} /> ) }
-        </Slider>
-      </div>
+  render () {
+    const {onClick, characters} = this.props
 
-      <div className="InviteGuide">
-        <Button color="inherit"
-                variant="outlined"
-                style={{borderColor: "#fff", fontFamily: "Sawarabi Mincho"}}
-                onClick={() => onClick(characters[currentIndex].id)} >
-          このキャラクターで始める
-        </Button>
-      </div>
-    </Page>
-  )
+    return (
+      <Page>
+        <div className="InviteSlider">
+          <Slider dots infinite speed={300} afterChange={this.handleSwipe}>
+            { characters.map(c => <Character key={c.id} character={c} /> ) }
+          </Slider>
+        </div>
+
+        <div className="InviteGuide">
+          <Button color="inherit"
+                  variant="outlined"
+                  style={{borderColor: "#fff", fontFamily: "Sawarabi Mincho"}}
+                  onClick={() => onClick(characters[this.state.index].id)} >
+            このキャラクターで始める
+          </Button>
+        </div>
+      </Page>
+    )
+  }
 }
--- src/components/Invited.js
+++ src/components/Invited.js
@@ -3,13 +3,12 @@ import React from 'react'
 import Page from './Page'
 import Character from '../models/Character'
 
-
 export default ({onClick, characterId}) => {
   const character = Character.create(characterId)
   return (
     <Page>
-      <div>
-        なにかパラメータ的なものが出る
+      <div className="PageHeader">
+        {character.name} が仲間に加わりました
       </div>
     </Page>
   )
